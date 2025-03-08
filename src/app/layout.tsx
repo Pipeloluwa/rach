b0ac0a84c73367141e38f4a100f8365675814922
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2025-03-08 11:56:52 +0100

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      
+
       <body
         className={`${arimaFont.className} antialiased overflow-hidden`}
       >
