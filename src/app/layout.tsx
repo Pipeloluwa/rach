500abbb2a8302b929f9f731873c23d7a194b864a
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2025-03-08 12:03:43 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head> */}
-
       <body
         className={`${arimaFont.className} antialiased overflow-hidden`}
       >
