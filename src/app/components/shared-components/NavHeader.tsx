566e56f7f1edd23839fd72f02622050900ef624a
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2025-08-18 15:48:24 +0100

    Update NavHeader.tsx

--- src/app/components/shared-components/NavHeader.tsx
+++ src/app/components/shared-components/NavHeader.tsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react'
 import LogoComponent from './LogoComponent'
 import Link from 'next/link'
 import { IActiveNav } from '@/app/interfaces/INavHeader';
+import { TiThMenu } from 'react-icons/ti';
+import { IoCloseSharp } from 'react-icons/io5';
+import { motion } from 'motion/react';
+import { animateFromRight } from '@/app/animation/animationVariants';
 
 
 
 
 export const NavHeader: React.FC<IActiveNav> = ({ activeTabValue }) => {
     const [activeTab, setActiveTab] = useState<string>("");
-    let logoUrls= new Map<string, string>();
+    let logoUrls = new Map<string, string>();
     logoUrls.set("home", "RG LOGO.png");
     logoUrls.set("about-us", "RG LOGO.png");
     logoUrls.set("project100", "PROJECT 100 LOGO_.png");
@@ -19,21 +23,114 @@ export const NavHeader: React.FC<IActiveNav> = ({ activeTabValue }) => {
         setActiveTab(activeTabValue);
     }, [activeTabValue]);
 
+
+
+    const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+
+    const openDrawer_ = () => {
+
+        setOpenDrawer(true);
+        document.body.style.overflow = "hidden";
+    }
+
+    const closeDrawer_ = () => {
+        setOpenDrawer(false);
+        document.body.style.overflow = "unset";
+    }
+
     return (
-        <ul className='z-50 h-[90px] flex justify-center items-center gap-x-4 sticky left-0 right-0 top-0 bg-black/50 backdrop-blur '>
-            <Link href={"/"} className={`${activeTab === "home" ? "text-gold font-bold" : "text-gray-300"} cursor-pointer`}>
-                {"Home"}
-            </Link>
+        <ul className='z-50 h-[90px] flex justify-center items-center gap-x-4 md:px-12 px-4 sticky left-0 right-0 top-0 bg-black/50 backdrop-blur '>
 
             <div className='w-[70px]'>
-                <LogoComponent path={`${logoUrls.get(activeTab)}`}/>
+                <LogoComponent path={`${logoUrls.get(activeTab)}`} />
             </div>
+
+
+
+
+
+            {
+                openDrawer
+                &&
+
+                <div onClick={() => { closeDrawer_() }} className='backdrop-blur-lg bg-black/60 lg:hidden flex z-50 h-screen w-screen fixed top-0 bottom-0 right-0 left-0 justify-end '>
+
+                    <motion.div
+                        onClick={(e) => { e.stopPropagation() }} 
+                        variants={animateFromRight}
+                        initial={"offscreen"}
+                        whileInView={"onscreen"}
+                        className='h-full w-[50%] flex flex-col gap-y-2 md:pl-16 pl-2 md:pr-[60px] pr-[16px] pt-5 rounded-l-2xl bg-gradient-to-b from-gray-800 to-black'
+                        >
+                        
+
+                        <div className='h-[52px] w-full flex justify-end'>
+
+                            <IoCloseSharp onClick={() => { closeDrawer_() }} className="cursor-pointer text-white size-[50px]" />
+
+                        </div>
+
+
+                        <div className='w-full flex flex-col'>
+                            <Link href={"/"} className={`${activeTab === "home" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                                {"Home"}
+                            </Link>
+
+                            <Link href={"/about-us"} className={`${activeTab === "about-us" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                                {"About Us"}
+                            </Link>
+
+                            <Link href={"/project-centum"} className={`${activeTab === "project100" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                                {"Project Centum"}
+                            </Link>
+                        </div>
+
+                    </motion.div>
+
+
+                </div>
+            }
+
+
+
+            <div className='w-full h-[52px] lg:hidden flex justify-end items-center'>
+                {
+                    !openDrawer
+                    && <TiThMenu onClick={() => { openDrawer_() }} className="cursor-pointer text-white size-[45px]" />
+                }
+            </div>
+
+
+
+            <div className='w-full lg:flex hidden justify-center items-center gap-x-2 px-4'>
+
+                <Link href={"/"} className={`${activeTab === "home" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                    {"Home"}
+                </Link>
+
+                <Link href={"/about-us"} className={`${activeTab === "about-us" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                    {"About Us"}
+                </Link>
+
+                <Link href={"/project-centum"} className={`${activeTab === "project100" ? "text-gold font-bold" : "text-gray-300 lg:hover:bg-gray-700"} rounded-xl block px-4 py-3 cursor-pointer`}>
+                    {"Project Centum"}
+                </Link>
+
+            </div>
+
+
+
+
+
+
+
+
 
             {/* <Link href={"/project-centum"} className={`${activeTab === "project100" ? "text-gold font-bold" : "text-gray-300"} cursor-pointer`}>
                 {"Project Centum"}
             </Link> */}
 
-            <li className="relative group">
+            {/* <li className="relative group">
                 <p className={`${activeTab === "project100" || activeTab === "about-us" ? "text-gold font-bold" : "text-gray-300"} cursor-pointer`}>
                     {"Pages"}
                 </p>
@@ -54,7 +151,7 @@ export const NavHeader: React.FC<IActiveNav> = ({ activeTabValue }) => {
                     </li>
 
                 </ul>
-            </li>
+            </li> */}
         </ul>
     )
 }
