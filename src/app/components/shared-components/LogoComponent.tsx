fa831a645e1dfd2088351239fe653d18d46aa7c1
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2025-08-18 16:19:05 +0100

    uodated

--- src/app/components/shared-components/LogoComponent.tsx
+++ src/app/components/shared-components/LogoComponent.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const LogoComponent: React.FC<{path: string}> = ({path}) => {
   return (
-    <Link href={path === "RG LOGO.png" ? `../` : ""}>
+    <Link href={path === "PROJECT 100 LOGO_.png" ? `/project-centum` : "./"}>
       <Image alt='logo' src={path === "undefined" ? '/images/logos/RG LOGO.png' : `/images/logos/${path}`} width={100} height={100} priority={true} className=' w-full h-full flex'>
       </Image>
     </Link>
--- src/app/components/shared-components/NavHeader.tsx
+++ src/app/components/shared-components/NavHeader.tsx
@@ -46,9 +46,6 @@ export const NavHeader: React.FC<IActiveNav> = ({ activeTabValue }) => {
             </div>
 
 
-
-
-
             {
                 openDrawer
                 &&
